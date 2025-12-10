84167fbbc3b5505b7a56a8109d759a1701b64c16
amirholya01 <amirholya01@gmail.com>
2025-12-11 00:22:59 +0330

    Initial commit

---
+++ main.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+function main(){
+    const app = express();
+    const port = 3000;
+
+    app.listen(port, () => {
+        console.log(`Server is running on http://localhost:${port}`);
+    });
+};
+
+main();
