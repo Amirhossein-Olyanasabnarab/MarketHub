03707f692b5504b26f27aeb3804dca43ee3be228
amirholya01 <amirholya01@gmail.com>
2025-12-11 00:30:46 +0330

    mongoose.config-connected to MongoDB

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ function main(){
     const app = express();
     const PORT = process.env.PORT;
 
+    require('./src/config/mongoose.config');
     app.listen(PORT, () => {
         console.log(`Server is running on http://localhost:${PORT}`);
     });
