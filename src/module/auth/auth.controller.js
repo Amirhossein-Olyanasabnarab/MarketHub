8330d20ccde37a8eb7afd44e0ec00bedcdb9bcaf
amirholya01 <amirholya01@gmail.com>
2025-12-11 16:14:08 +0330

    send otp controller

--- src/module/auth/auth.controller.js
+++ src/module/auth/auth.controller.js
@@ -1,7 +1,27 @@
 const autoBind = require("auto-bind");
+const {AuthService} = require('./auth.service');
 class AuthController{
+    #service;
     constructor(){
         autoBind(this);
+        this.#service = AuthService;
+    }
+
+    async sendOtp(req,res,next){
+        try{
+            const {mobile} = req.body;
+            const result = await this.#service.sendOtp(mobile);
+            res.status(200).json({
+                status:200,
+                success:true,
+                message:"Otp sent successfully!",
+                data:{
+                    mobile: result.mobile
+                }
+            });
+        }catch(err){
+            next(err);
+        }
     }
 }
 
