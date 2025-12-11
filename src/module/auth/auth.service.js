698258add6748ee3e69b62c2159abc7f16a07b14
amirholya01 <amirholya01@gmail.com>
2025-12-11 16:12:11 +0330

    chekOtp service

--- src/module/auth/auth.service.js
+++ src/module/auth/auth.service.js
@@ -8,7 +8,26 @@ class AuthService{
     }
 
     async sendOtp(mobile){
+        const user = await this.existByMobile(mobile);
 
+        const now = new Date().getTime();
+        const otp = {
+            code: Math.floor(1000 + Math.random() * 9000),
+            expireIn: now + (1000 * 60 * 2) // 2 minutes
+        };
+       
+        if(!user){
+            const newUser = await this.#model.create({mobile,otp});
+            return newUser;
+        }
+
+        if(user.otp && user.expireIn > now){
+            throw {status:400,message:"You can request new otp after 2 minutes!"};
+        }
+
+        user.otp = otp;
+        await user.save();
+        return user;
     }
 
     async checkExistByMobile(mobile){
