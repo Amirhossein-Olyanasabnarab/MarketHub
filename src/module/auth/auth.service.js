a35fad258b78d9808c777040723e327509d476e1
amirholya01 <amirholya01@gmail.com>
2025-12-11 16:05:38 +0330

    CheckExistByMobile

--- src/module/auth/auth.service.js
+++ src/module/auth/auth.service.js
@@ -1,7 +1,21 @@
 const autoBind = require("auto-bind");
+const {UserModel} = require('../user/user.model');
 class AuthService{
+    #model;
     constructor(){
         autoBind(this);
+        this.#model = UserModel;
+    }
+
+    async sendOtp(mobile){
+
+    }
+
+    async checkExistByMobile(mobile){
+        const user = await this.#model.findOne({mobile});
+        if(!user) throw {status:404,message:"User not found!"};
+        return user;
+        
     }
 }
 module.exports ={
