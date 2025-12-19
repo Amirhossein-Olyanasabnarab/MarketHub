dbcdb968d5a146bb2b5ce26e7158f06ae7d695d2
amirholya01 <amirholya01@gmail.com>
2025-12-19 12:15:27 +0330

    Making the first connection to the server

---
+++ main.js
@@ -0,0 +1,13 @@
+const express = require('express');
+require('dotenv').config();
+
+async function main(){
+    const app = express();
+    const PORT = process.env.PORT;
+
+    app.listen(PORT, () => {
+        console.log(`Server is running on http://localhost:${PORT}`)
+    })
+};
+
+main();
