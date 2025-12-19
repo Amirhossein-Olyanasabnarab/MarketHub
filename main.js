e775f7533b6e624520bfc3529e25ea7a95ce34c1
amirholya01 <amirholya01@gmail.com>
2025-12-19 12:49:16 +0330

    install morgan and config express json

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const setupSwagger = require("./src/config/swagger.config");
+const morgan = require("morgan");
 require("dotenv").config();
 
 async function main() {
   const app = express();
   const PORT = process.env.PORT;
 
+  app.use(morgan("dev"));
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
   require("./src/config/mongoose.config");
   setupSwagger(app);
   app.listen(PORT, () => {
